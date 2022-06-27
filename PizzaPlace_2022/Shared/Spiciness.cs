97703920e1583e43e3763a287913a8ed12994128
Admin <Admin@SYSCEO-N1BI2CQL>
2022-06-27 11:32:14 +0700

    Add project files.

---
+++ PizzaPlace_2022/Shared/Spiciness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPlace_2022.Shared {
+    public enum Spiciness {
+        None,
+        Spicy,
+        Hot,
+        gt1,gt2,gt3,gt4,gt5,gt6,gt7
+    }
+}
